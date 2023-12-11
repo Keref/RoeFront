18a6617f5a8970585f13d0ce431fd9701332e01c
caviar-roe <caviar@roe.finance>
2023-12-11 12:34:43 +0100

    Revert "fix: update web3Onboar version and config"
    This reverts commit 12f9cfc832b304aa351ea551978bfc66b8ec3da3.

--- web3Onboard.ts
+++ web3Onboard.ts
@@ -1,27 +1,64 @@
-import injectedModule, { ProviderLabel } from '@web3-onboard/injected-wallets';
 import { init } from '@web3-onboard/react';
+import injectedModule, { ProviderIdentityFlag, ProviderLabel } from '@web3-onboard/injected-wallets';
 
-const supportedWalletsLabels = [
-    ProviderLabel.OKXWallet,
-    ProviderLabel.MetaMask,
-    ProviderLabel.Rabby,
-];
+const Metamask = {
+    label: 'Metamask',
+    injectedNamespace: 'ethereum',
+    checkProviderIdentity: ({ provider }) => !!provider && !!provider[ProviderIdentityFlag.MetaMask],
+    getIcon: async () => (await import('./node_modules/@web3-onboard/injected-wallets/dist/icons/metamask')).default,
+    getInterface: () => ({
+        provider: window['ethereum']
+    }),
+    platforms: ['desktop', 'mobile']
+};
 
-const supportedWalletsFilter = Object.fromEntries(
-    Object.entries(ProviderLabel).map(([key, value]) => [
-        key,
-        supportedWalletsLabels.includes(value),
-    ])
-);
+const Rabby = {
+    label: 'Rabby',
+    injectedNamespace: 'ethereum',
+    checkProviderIdentity: ({ provider }) => !!provider && !!provider[ProviderIdentityFlag.Rabby],
+    getIcon: async () => (await import('./node_modules/@web3-onboard/injected-wallets/dist/icons/rabby')).default,
+    getInterface: () => ({
+        provider: window['ethereum']
+    }),
+    platforms: ['desktop']
+};
 
-const injectedWalletsModule = injectedModule({
-    filter: supportedWalletsFilter,
+const OkxWallet = {
+    label: 'OKX Wallet',
+    injectedNamespace: 'ethereum',
+    checkProviderIdentity: ({ provider }) => !!provider && !!provider[ProviderIdentityFlag.OKXWallet],
+    getIcon: async () => (await import('./node_modules/@web3-onboard/injected-wallets/dist/icons/okxwallet')).default,
+    getInterface: () => ({
+        provider: window['ethereum']
+    }),
+    platforms: ['desktop']
+};
+
+const injected = injectedModule({
+    custom: [
+        Metamask,
+        OkxWallet,
+        Rabby
+    ],
+    displayUnavailable: true,
+    sort: (wallets) => {
+        const okxwallet = wallets.find(({ label }) => label === ProviderLabel.OKXWallet)
+        const metaMask = wallets.find(({ label }) => label === ProviderLabel.MetaMask)
+        const rabby = wallets.find(({ label }) => label === ProviderLabel.Rabby)
+
+        return [
+            okxwallet,
+            metaMask,
+            rabby
+        ]
+    }
 });
 
 export default init({
     theme: 'dark',
-    wallets: [injectedWalletsModule],
-
+    wallets: [
+        injected,
+    ],
     chains: [
         {
             id: 42161,
@@ -30,43 +67,36 @@ export default init({
             rpcUrl: 'https://endpoints.omniatech.io/v1/arbitrum/one/public'
         },
     ],
-
     connect: {
-        iDontHaveAWalletLink: 'https://okx.com/web3',
-        autoConnectLastWallet: true,
+        iDontHaveAWalletLink: 'https://www.okx.com/web3',
+        autoConnectAllPreviousWallet: true
     },
-
     accountCenter: {
         desktop: {
             enabled: false,
         },
-
         mobile: {
             enabled: false,
-        },
+        }
     },
-
     appMetadata: {
         name: 'Good Entry',
         icon: '<svg></svg>',
-        logo: '/Good Entry Logo.svg',
+        logo: '<svg></svg>',
         description: 'Good Entry',
-
         recommendedInjectedWallets: [
             {
                 name: 'OKX Wallet',
-                url: 'https://okx.com/web3',
+                url: 'https://www.okx.com/web3'
             },
             {
                 name: 'MetaMask',
-                url: 'https://metamask.io',
+                url: 'https://metamask.io'
             },
             {
                 name: 'Rabby',
-                url: 'https://rabby.io',
-            }
+                url: 'https://rabby.io/'
+            },
         ],
-    },
-
-    disableFontDownload: true,
+    }
 });
